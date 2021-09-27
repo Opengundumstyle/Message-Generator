3bf15d182192702e56d172de9f0dd650a2a60fa7
Opengundumstyle <zhlin04@gmail.com>
2021-09-26 20:40:01 -0700

    make a function change that grab four random sentences from my txt file.

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
+
+// conert sentences from text file into array
 var fs=require('fs');
 var text=fs.readFileSync("./pick_up_lines.txt",'utf-8');
-var textByLine = text.split('\n')
+var textByLine = text.split('\n');
 
-console.log(textByLine);
+
+const pickRandomLines=(pickupLines)=>{
+    let ranArry=[];
+    let i=1;
+    while(i<5){
+    ranArry.push(pickupLines[Math.floor(Math.random()*pickupLines.length)])
+    i++
+        }
+    return ranArry;
+
+};
+    
+
+
+console.log(pickRandomLines(textByLine))
+
